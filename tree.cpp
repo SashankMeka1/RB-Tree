408fbed3963054729521ea1b737db772aac2551d
Sashank <sashankmeka7@gmail.com>
2021-05-24 11:59:37 -0700

    citation

--- tree.cpp
+++ tree.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cstdlib>
+//source: https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-046j-introduction-to-algorithms-sma-5503-fall-2005/video-lectures/lecture-10-red-black-trees-rotations-insertions-deletions/lec10.pdf
 using namespace std;
 void tree::read_file(char * file_name){
 	FILE * file;
